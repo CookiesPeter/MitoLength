80158ccdb4a94bfff8b7d94e69a5af30eded6183
CookiesPeter <134918430+CookiesPeter@users.noreply.github.com>
2023-07-31 10:07:58 +0800

    Add files via upload

--- MitolengthDebug.py
+++ MitolengthDebug.py
@@ -2,29 +2,44 @@ import pandas as pd
 import numpy as np
 from scipy.signal import argrelextrema, find_peaks, filtfilt, butter, peak_prominences
 from matplotlib import pyplot as plt
-from matplotlib import cm
-from matplotlib.ticker import LinearLocator
-import csv
 import czifile as cz
 import xml.etree.ElementTree as ET
 import tkinter.filedialog as tk
 from sklearn.linear_model import LinearRegression
-
-
-
+import time
 
 #define local minima before peak
-def detect_local_minima_before_peaks(signal, peak_indices,htres):
+def detect_local_minima_before_peaks(signal, peak_indices):
     local_minima = []
     for peak_index in peak_indices:
         before_peak = signal[:peak_index]
-        before_peak = before_peak[before_peak < htres]
         local_min_index = argrelextrema(before_peak,np.less)[0]
         if not local_min_index.any():
             continue
         local_min_index = local_min_index[-1]
         local_minima.append(local_min_index)
     return local_minima
+
+#define butter_lowpass_filtfilt
+def butter_lowpass_filtfilt(data,fre,order=10):
+    b,a = butter(order,fre,'lowpass', analog=False)
+    output = filtfilt(b,a,data,axis=0)
+    return output
+
+#plot 3d
+def plot3d(X,Y,Z,Title):
+    #plotting
+    fig = plt.figure()
+    ax = fig.add_subplot(projection='3d')
+
+    # Plot the surface
+    ax.scatter(X, Y, Z,c=Z)
+    plt.title(Title)
+    ax.set_xlabel('Cutoff Frequency')
+    ax.set_ylabel('Prominence')
+    ax.set_zlabel(Title)
+    plt.savefig(Title)
+    plt.show()
 
 #get metadata from czi
 xml_metadata = cz.CziFile("C:/Users/Ludwig.Qi/Desktop/POON Lab first analysis/Testing Single Sample/230414_LK_MH_PF_AcquisitionBlock3_pt3-Scene-26-P1-C05.czi").metadata()
@@ -35,7 +50,6 @@ for val in root.findall('.//Distance[@Id="X"]/Value'):
 
 #data import and tidying
 filepath=tk.askopenfilenames(title='Please select the csv file from TrackMate.',filetypes=(('Csv','*.csv'),('All files','*')))
-
 df=pd.read_csv(filepath[0])
 
 #Drop some useless labels
@@ -50,56 +64,45 @@ df.POSITION_Y = df.POSITION_Y.astype(float)
 
 #set bound
 bound = pixel_size_in_microns-10
-df = df.drop(df[(df.POSITION_X < 10) & (df.POSITION_X > bound)].index)
-df = df.drop(df[(df.POSITION_Y < 10) & (df.POSITION_Y > bound)].index)
+df.drop(df[df.POSITION_X < 10].index,axis=0,inplace=True)
+df.drop(df[df.POSITION_X > bound].index,axis=0,inplace=True)
+df.drop(df[df.POSITION_Y < 10].index,axis=0,inplace=True)
+df.drop(df[df.POSITION_Y > bound].index,axis=0,inplace=True)
 
-#append/start a csv file, set initial indices, add a header
+#set initial indices
 ind=0
 falsepositive=0
 miss_count =0
 normal=0
-file = open('Results.csv','a',newline='')
-writer=csv.writer(file)
-head=['Index','TrackID','#splits','Algorithm Start','Algorithm End','Prominences','Adaptive Threshold']
-writer.writerow(head)
-file.close()
-
-#add a cheat code
-skip=int(input('Please indicate the track Id you wanna skip to:'))
 normal_list=[]
 falsep_list=[]
 miss_list=[]
 normal_rate_list=[]
 r_list=[]
-qnum_list=[]
+freq_list=[]
 promnum_list=[]
-for qnum in range(0,101,20):
-    
-    for promnum in range(0,468,94):
-        print("Quantile: ",qnum,"Prominence:",promnum)
+idlist=[]
+
+for freq in range(5,10,1):
+    for promnum in range(0,301,25):
+        print("cutoff freq: ",freq/10,"Prominence:",promnum)
         falsepositive=0
         miss_count =0
         normal=0
-        qnum_list.append(qnum)
+        freq_list.append(freq/10)
         promnum_list.append(promnum)
-        Manual_Start_list=[]
-        Manual_End_list=[]
-        Algorithm_Start_list=[]
-        Algorithm_End_list=[]
+        All_start_list_manual=[]
+        All_end_list_manual=[]
+        All_start_list_alg=[]
+        All_end_list_alg=[]
 
-        
         #Search unique TRACK ID
         for id in df.index.unique():
 
-            #skip code
-            if id < skip:
-                continue
-
             #obtain Frame, Std data, sort from a specific track ID
             newdf=df.loc[id,['FRAME','STD_INTENSITY_CH1']].sort_values(by='FRAME',ascending=True)
-            dfm=pd.read_excel("C:\\Users\\Ludwig.Qi\\Desktop\\POON Lab first analysis\\Testing Single Sample\\Manual_Data_Collection..xlsx")
+            dfm=pd.read_excel("Debugging Materials/Manual_Data_Collection.xlsx")
             manualdata=dfm.loc[id]
-
             x = newdf.STD_INTENSITY_CH1.values.astype(float)
 
             #Screen for tracks with length more than 50 frames
@@ -108,18 +111,13 @@ for qnum in range(0,101,20):
                     miss_count=miss_count+1
                 if manualdata["MS1"]!="None" and manualdata["MS2"]!="None":
                     miss_count=miss_count+2
-
-
                 continue
             
             #Smoothening the curve by filtfilt
-            b, a = butter(8, 0.125,analog=False)
-            yy= filtfilt(b,a,x)
+            yy=butter_lowpass_filtfilt(x,fre=freq/10)
             #find peaks and threshold
-            threshold = np.maximum(200,np.quantile(yy,qnum/100))
-            peaks,_ = find_peaks(yy,height=threshold,distance=60,prominence=promnum)
+            peaks,_ = find_peaks(yy,distance=60,prominence=promnum)
             
-
             #give up if no peaks identified
             if not peaks.size:
                 if manualdata["MS1"]!="None" and manualdata["MS2"]=="None":
@@ -127,30 +125,21 @@ for qnum in range(0,101,20):
                 if manualdata["MS1"]!="None" and manualdata["MS2"]!="None":
                     miss_count=miss_count+2
                 continue
-            #give up if median is above mean
-            #if np.median(yy) > np.mean(yy):
-                #continue
             
             #Find local maximum with smoothened curve
-            local_minima = detect_local_minima_before_peaks(x, peaks,np.quantile(x,0.85))
-            prominences= peak_prominences(yy,peaks)[0]
+            local_minima = detect_local_minima_before_peaks(x, peaks)
 
-            #for i,peakpair in enumerate(local_minima):
-                #((index,values),peakss) = peakpair
-                #file =open('Results.csv','a',newline='')
-                #writer=csv.writer(file)
-                #ind=ind+1
-                #Append=[[str(ind),str(id),str(len(peaks)),str(index),str(peakss),str(prominences[i]),str(np.quantile(x,0.95))]]
-                #writer.writerows(Append)
-                #file.close()
             if not len(local_minima):
                 continue
             
             for i,ipeak in enumerate(peaks):
                 if min(local_minima)>ipeak:
                     peaks = np.delete(peaks,i)
+            #manual None
             if manualdata["MS1"]=="None":
                 falsepositive = falsepositive +len(peaks)
+
+            #Manual One
             if manualdata["MS1"]!="None" and manualdata["MS2"]=="None":
                 if manualdata["ME1"]=="None":
                     continue
@@ -165,12 +154,15 @@ for qnum in range(0,101,20):
                             differ=abs(peaks[i]-manualdata["ME1"])
                             real_peak=peaks[i]
                             index1=i
-                    Manual_End_list.append(manualdata["ME1"])
-                    Algorithm_End_list.append(real_peak)
-                    Manual_Start_list.append(manualdata["MS1"])
-                    Algorithm_Start_list.append(local_minima[index1])
+                    All_end_list_manual.append(manualdata["ME1"])
+                    All_end_list_alg.append(real_peak)
+                    All_start_list_manual.append(manualdata["MS1"])
+                    All_start_list_alg.append(local_minima[index1])
                     falsepositive=falsepositive+len(peaks)-1
                     normal=normal+1
+                    idlist += [id]
+            
+            #Manual TWO
             if manualdata["MS1"]!="None" and manualdata["MS2"]!="None":
                 if manualdata["ME2"]=="None":
                     continue
@@ -179,16 +171,17 @@ for qnum in range(0,101,20):
                 if len(peaks)==1:
                     miss_count=miss_count+1
                     if abs(peaks[0]- manualdata["ME1"])<=abs(peaks[0]-manualdata["ME2"]):
-                        Manual_End_list.append(manualdata["ME1"])
-                        Algorithm_End_list.append(peaks[0])
-                        Manual_Start_list.append(manualdata["MS1"])
-                        Algorithm_Start_list.append(local_minima[0])
+                        All_end_list_manual.append(manualdata["ME1"])
+                        All_end_list_alg.append(peaks[0])
+                        All_start_list_manual.append(manualdata["MS1"])
+                        All_start_list_alg.append(local_minima[0])
                     if abs(peaks[0]- manualdata["ME1"])>abs(peaks[0]-manualdata["ME2"]):
-                        Manual_End_list.append(manualdata["ME2"])
-                        Algorithm_End_list.append(peaks[0])
-                        Manual_Start_list.append(manualdata["MS2"])
-                        Algorithm_Start_list.append(local_minima[0])
+                        All_end_list_manual.append(manualdata["ME2"])
+                        All_end_list_alg.append(peaks[0])
+                        All_start_list_manual.append(manualdata["MS2"])
+                        All_start_list_alg.append(local_minima[0])
                     normal=normal+1
+                    idlist += [id]
                 if len(peaks)>1:
                     differ = abs(peaks[0]-manualdata["ME1"])
                     real_peak=peaks[0]
@@ -198,10 +191,10 @@ for qnum in range(0,101,20):
                             differ=abs(peaks[j]-manualdata["ME1"])
                             real_peak=peaks[j]
                             index2=j
-                    Manual_End_list.append(manualdata["ME1"])
-                    Algorithm_End_list.append(real_peak)
-                    Manual_Start_list.append(manualdata["MS1"])
-                    Algorithm_Start_list.append(local_minima[index2])
+                    All_end_list_manual.append(manualdata["ME1"])
+                    All_end_list_alg.append(real_peak)
+                    All_start_list_manual.append(manualdata["MS1"])
+                    All_start_list_alg.append(local_minima[index2])
                     differ = abs(peaks[0]-manualdata["ME2"])
                     real_peak=peaks[0]
                     index3=0
@@ -210,79 +203,46 @@ for qnum in range(0,101,20):
                             differ=abs(peaks[j]-manualdata["ME2"])
                             real_peak=peaks[j]
                             index3=j
-                    Manual_End_list.append(manualdata["ME2"])
-                    Algorithm_End_list.append(real_peak)
-                    Manual_Start_list.append(manualdata["MS2"])
-                    Algorithm_Start_list.append(local_minima[j])
+                    All_end_list_manual.append(manualdata["ME2"])
+                    All_end_list_alg.append(real_peak)
+                    All_start_list_manual.append(manualdata["MS2"])
+                    All_start_list_alg.append(local_minima[j])
                     falsepositive=falsepositive+len(peaks)-2
                     normal=normal+2
-            
-            
-        All_start_list_manual=Manual_Start_list
-        All_end_list_manual=Manual_End_list
-        All_start_list_alg=Algorithm_Start_list
-        All_end_list_alg=Algorithm_End_list
+                    idlist += 2 * [id]
+                
+        #give it a break
+        time.sleep(1)
         
-
-
-
         data_start =pd.DataFrame({'manual_start':All_start_list_manual,'Algo_start':All_start_list_alg})
-        data_manual_start=np.array(data_start['manual_start']).reshape((-1,1))
-        data_algo_start=np.array(data_start['Algo_start'])
+        data_manual_start=np.array(data_start['manual_start'])
+        data_algo_start=np.array(data_start['Algo_start']).reshape((-1,1))
         regr_start=LinearRegression()
-        regr_start.fit(data_manual_start,data_algo_start)
+        regr_start.fit(data_algo_start,data_manual_start)
         data_end=pd.DataFrame({'manual_end':All_end_list_manual,'Algo_end':All_end_list_alg})
-        data_manual_end=np.array(data_end['manual_end']).reshape((-1,1))
-        data_algo_end=np.array(data_end['Algo_end'])
+        data_manual_end=np.array(data_end['manual_end'])
+        data_algo_end=np.array(data_end['Algo_end']).reshape((-1,1))
         regr_end=LinearRegression()
-        regr_end.fit(data_manual_end,data_algo_end)
-        r_square=regr_end.score(data_manual_end,data_algo_end)*0.5+regr_start.score(data_manual_start,data_algo_start)*0.5
+        regr_end.fit(data_algo_end,data_manual_end)
+        r_square=regr_end.score(data_algo_end,data_manual_end)*1+regr_start.score(data_algo_start,data_manual_start)*0
         normal_rate=float(normal)/(normal+falsepositive+miss_count)
-
+        print("R square: "+str(r_square)+"\nTracked normal cell#: "+str(normal))
         r_list.append(r_square)
         normal_rate_list.append(normal_rate)
         normal_list.append(normal)
         falsep_list.append(falsepositive)
         miss_list.append(miss_count)
 
-all_data={"Quantile":qnum_list,"Prominance":promnum_list,"R_Square":r_list,"Normal_rate":normal_rate_list,
+all_data={"Cutoff Frequency":freq_list,"Prominance":promnum_list,"R_Square":r_list,"Normal_rate":normal_rate_list,
           "Normal":normal_list,"Falsepos":falsep_list,"Missing":miss_list}
 all_df=pd.DataFrame(all_data)
 all_df.to_csv("data.csv")
-#plotting
-fig = plt.figure()
-ax = fig.add_subplot(projection='3d')
 
-# Make data.
-X = qnum_list
-Y = promnum_list
-Z= r_list
+#Rsquare
+plot3d(freq_list,promnum_list,r_list,"R square")
 
-# Plot the surface.
-surf = ax.scatter(X, Y, Z,c=Z)
-plt.title('Rsquare mapping')
-ax.set_xlabel('Quantile')
-ax.set_ylabel('Prominence')
-ax.set_zlabel('R Square')
+#NormalRate
+plot3d(freq_list,promnum_list,normal_rate_list,"Normal Rate")
 
-plt.show()
-
-#plotting
-fig = plt.figure()
-ax = fig.add_subplot(projection='3d')
-
-# Make data.
-X = qnum_list
-Y = promnum_list
-Z= normal_rate_list
-# Plot the surface.
-surf = ax.scatter(X, Y, Z,c=Z)
-plt.title('Normal Proportion')
-ax.set_xlabel('Quantile')
-ax.set_ylabel('Prominence')
-ax.set_zlabel('Normal Proportion')
-
-
-plt.show()
-
-
+#CellNumber
+plot3d(freq_list,promnum_list,normal_list,"Cell Number")
